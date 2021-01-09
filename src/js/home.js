d30c46bb198e39689c12121107931be8778be71f
Angel Gonzalez <agb92@live.com>
2021-01-08 23:59:45 -0600

    selectores

--- src/js/home.js
+++ src/js/home.js
@@ -75,7 +75,28 @@ fetch('https://randomuser.me/api/dsfdsfsd')
     const data = await response.json()
     return data;
   }
-  const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
-  const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
-  const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
+  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action')
+  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama')
+  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation')
   console.log(actionList, dramaList, animationList)
+
+  const $actionContainer = document.querySelector('#action');
+  const $dramaContainer = document.getElementById('#drama');
+  const $animationContainer = document.getElementById('#animation');
+
+
+  const $featuringContainer = document.getElementById('#featuring');
+  const $form = document.getElementById('#form');
+  const $home = document.getElementById('#home');
+
+
+  // const $home = $('.home .list #item');
+  const $modal = document.getElementById('modal');
+  const $overlay = document.getElementById('overlay');
+  const $hideModal = document.getElementById('hide-modal');
+
+  const $modalTitle = $modal.querySelector('h1');
+  const $modalImage = $modal.querySelector('img');
+  const $modalDescription = $modal.querySelector('p');
+
+})()
