78b2f74cb8186eb34f7f63affd9d1fccdb40f004
Angel Gonzalez <agb92@live.com>
2021-01-15 23:24:56 -0600

    formularios

--- src/js/home.js
+++ src/js/home.js
@@ -84,7 +84,24 @@ fetch('https://randomuser.me/api/dsfdsfsd')
       $element.setAttribute(attribute, attributes[attribute]);
     }
   }
-  $form.addEventListener('submit', (event) => {
+  const BASE_API = 'https://yts.mx/api/v2/';
+
+  function featuringTemplate(peli) {
+    return (
+      `
+      <div class="featuring">
+        <div class="featuring-image">
+          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${peli.title}</p>
+        </div>
+      </div>
+      `
+    )
+  }
+  $form.addEventListener('submit', async (event) => {
     // debugger
     event.preventDefault();
     $home.classList.add('search-active')
@@ -95,13 +112,17 @@ fetch('https://randomuser.me/api/dsfdsfsd')
       width: 50,
     })
     $featuringContainer.append($loader);
+    const data = new FormData($form);
+    const peli = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
+    const HTMLString = featuringTemplate(peli.data.movies[0]);
+    $featuringContainer.innerHTML = HTMLString;
   })
   // const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
   // const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
   // const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
-  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action')
-  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama')
-  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation')
+  const actionList = await getData(`${BASE_API}list_movies.json?genre=action`)
+  const dramaList = await getData(`${BASE_API}list_movies.json?genre=drama`)
+  const animationList = await getData(`${BASE_API}list_movies.json?genre=animation`)
   console.log(actionList, dramaList, animationList)
 
   function videoItemTemplate(movie) {
